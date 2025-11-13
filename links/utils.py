d02c052ef9554866046e02857f96481bce32745f
Mario Cornejo <mariocesarcornejo@gmail.com>
2025-11-13 01:21:40 -0300

    change url validation

--- links/utils.py
+++ links/utils.py
@@ -1,6 +1,9 @@
-import re
+from urllib.parse import urlparse  # ← Viene con Python
 
 
 def is_valid_url(url):
-    url_pattern = r'^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$'
-    return re.match(url_pattern, url, re.IGNORECASE) is not None
+    try:
+        result = urlparse(url)
+        return all([result.scheme in ['http', 'https'], result.netloc])
+    except:
+        return False
